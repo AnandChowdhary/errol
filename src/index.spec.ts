57aa44050fd09bf8a876695b8b01ac05a304ec12
Anand Chowdhary <mail@anandchowdhary.com>
2021-01-22 21:28:32 +0530

    :tada: Init @koj-co repo

---
+++ src/index.spec.ts
@@ -0,0 +1,11 @@
+/**
+ * @copyright Koj <https://koj.co>
+ * @link https://github.com/koj-co/template
+ * 
+*/
+
+import { hello } from "./";
+
+test("hello world", () => {
+  expect(hello).toBe("world");
+});
