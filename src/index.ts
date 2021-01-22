ce2dd14415e1b0e4388f14dbc551d38584198e29
Anand Chowdhary <mail@anandchowdhary.com>
2021-01-22 22:14:40 +0530

    :sparkles: Add support for Slack

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,10 @@ import {
   ErrolEmailConfiguration,
   ErrolNotification,
   ErrolServiceConfiguration,
+  ErrolSlackConfiguration,
 } from "./interfaces";
 import { createTransport } from "nodemailer";
+import { WebClient } from "@slack/web-api";
 
 export class Errol {
   constructor(private configuration: ErrolServiceConfiguration[]) {}
@@ -17,7 +19,16 @@ export class Errol {
   async send(notification: ErrolNotification) {
     for await (const item of this.configuration) {
       if (item.service === ErrolService.EMAIL) await this.email(item.config, notification);
+      else if (item.service === ErrolService.SLACK) await this.slack(item.config, notification);
     }
+  }
+
+  private async slack(config: ErrolSlackConfiguration, notification: ErrolNotification) {
+    const client = new WebClient(config.token);
+    return client.chat.postMessage({
+      text: notification.text,
+      channel: config.channelId,
+    });
   }
 
   private async email(config: ErrolEmailConfiguration, notification: ErrolNotification) {
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -4,12 +4,23 @@ export interface ErrolNotification {
 
 export enum ErrolService {
   EMAIL,
+  SLACK,
 }
 
-export type ErrolServiceConfiguration = {
-  service: ErrolService;
-  config: ErrolEmailConfiguration;
-};
+export type ErrolServiceConfiguration =
+  | {
+      service: ErrolService.EMAIL;
+      config: ErrolEmailConfiguration;
+    }
+  | {
+      service: ErrolService.SLACK;
+      config: ErrolSlackConfiguration;
+    };
+
+export interface ErrolSlackConfiguration {
+  token: string;
+  channelId: string;
+}
 
 export interface ErrolEmailConfiguration {
   from: string;
